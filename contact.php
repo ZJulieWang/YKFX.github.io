f984e8e9cfa71dcef9115d4be92157e0ec1e21d0
ZJulieWang <wangzhengjuppxia@gmail.com>
2025-04-17 17:47:13 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,23 +1,37 @@
 <?php
 if (isset($_POST['submit'])) { 
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $ToEmail = 'test@test.com';
-      $EmailSubject = $_POST['subject']; 
-      $mailheader = "From: ".$_POST["email"]."\r\n"; 
-      $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
-      $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-      $MESSAGE_BODY = "Name: ".$_POST["name"]."<br>"; 
-      $MESSAGE_BODY .= "Email: ".$_POST["email"]."<br>"; 
-      $MESSAGE_BODY .= "Subject:".$_POST['subject']."<br />";  
-      $MESSAGE_BODY .= "Message: ".nl2br($_POST["message"])."<br>"; 
-      if(mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader))
-      {
-      echo "<script>alert('Mail was sent !');</script>";
-      echo "<script>document.location.href='http://localhost/creativekitten/'</script>";
-      }
-      else
-      {
-      echo "<script>alert('Mail was not sent. Please try again later');</script>";
-      }
-     }
+    // 清理并获取输入
+    $name    = htmlspecialchars(trim($_POST['name']));
+    $email   = htmlspecialchars(trim($_POST['email']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
+    // 检查邮箱格式是否合法
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<script>alert('Invalid Email Address');</script>";
+        exit;
+    }
+
+    // 设置接收邮箱地址（请替换成你自己的）
+    $toEmail = 'wangyi9003@gmail.com';
+
+    // 邮件头部设置
+    $headers  = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    // 邮件正文
+    $body  = "Name: $name<br>";
+    $body .= "Email: $email<br>";
+    $body .= "Subject: $subject<br>";
+    $body .= "Message:<br>" . nl2br($message);
+
+    // 尝试发送邮件
+    if (mail($toEmail, $subject, $body, $headers)) {
+        echo "<script>alert('Email Sent!');</script>";
+        echo "<script>window.location.href='index.html';</script>";
+    } else {
+        echo "<script>alert('Unsuccessful! Please Try Again.');</script>";
+    }
+}
+?>
